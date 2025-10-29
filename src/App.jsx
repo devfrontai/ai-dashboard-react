8eda2155be5e82a932c745184f3ea6fc169d8b17
devfrontai <wealhtun@gmail.com>
2025-10-29 05:11:05 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Dashboard de IA</h1>
+      <p>React + Groq API funcionando 🚀</p>
+    </div>
+  );
+}
+
+export default App;
