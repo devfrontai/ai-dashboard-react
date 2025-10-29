2f3684a1d90cba6b686e142bed0864ed87e52706
devfrontai <wealhtun@gmail.com>
2025-10-29 04:49:17 -0300

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Meu Dashboard de IA</h1>
+      <p>Estrutura mínima funcionando.</p>
+    </div>
+  )
+}
